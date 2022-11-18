b52fbb53cf9c37d7cb2c167d76f43329138fb547
gmh5225 <2315157@qq.com>
2022-11-18 23:01:19 +0800

    Update

--- llvm-msvc-testing/Source.cpp
+++ llvm-msvc-testing/Source.cpp
@@ -1,5 +1,9 @@
 #include <Windows.h>
+#include <stdio.h>
 #include <lazy_importer/include/lazy_importer.hpp>
+
+#pragma comment(linker, "/SECTION:.CODE,ERW")
+__declspec(allocate(".CODE")) int a_CodData{0xAA};
 
 void
 test_lazy_importer()
@@ -12,6 +16,7 @@ int
 main()
 {
     test_lazy_importer();
+    printf("a_CodData=%x\n", a_CodData);
     system("pause");
     return 0;
 }
