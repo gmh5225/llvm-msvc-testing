cfddf730453b097d41d0025031eb5851cdd8eefc
gmh5225 <2315157@qq.com>
2022-11-18 23:22:55 +0800

    Update

--- llvm-msvc-testing/Source.cpp
+++ llvm-msvc-testing/Source.cpp
@@ -5,6 +5,15 @@
 #pragma comment(linker, "/SECTION:.CODE,ERW")
 __declspec(allocate(".CODE")) int a_CodData{0xAA};
 
+#pragma code_seg(push, label, ".CODE")
+DECLSPEC_NOINLINE
+void
+test_conflict_section()
+{
+    printf("hi\n");
+}
+#pragma code_seg(pop, label)
+
 void
 test_lazy_importer()
 {
@@ -16,6 +25,7 @@ int
 main()
 {
     test_lazy_importer();
+    test_conflict_section();
     printf("a_CodData=%x\n", a_CodData);
     system("pause");
     return 0;
